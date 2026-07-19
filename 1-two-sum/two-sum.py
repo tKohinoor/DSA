b4090910684ea239e421e73ab5d2455b18fffe4e
Kohinoor Tiwari <142927055+tKohinoor@users.noreply.github.com>
2026-07-20 00:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (17.58%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in mp:
+                return [mp[complement], i]
+            mp[num] = i
